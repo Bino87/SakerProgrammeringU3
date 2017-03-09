b1494ba1e1ff705ce02a968cfcbfda2d28e9e3f0
Daddyfuzzynuts <daniel.vinberg@tele2.se>
2017-03-09 15:35:48 +0100

    DatabaseInterface Added

---
+++ SickleaveReporting/DatabaseInterface/Excel/Excel.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Data.OleDb;
+using Utilities;
+namespace DatabaseInterface.Excel
+{
+    public static class Excel
+    {
+        public static bool CheckForUser(string userName, string password)
+        {
+            userName = StringManipulation.Neutralize(userName);
+            password = password.Replace("'", "");
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source='Database.xls';Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1;\" ";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                using (OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Users$] WHERE Username = '" + userName + "' AND Password='" + password + "'", connection))
+                {
+                    var userFound = cmd.ExecuteReader();
+                    if (userFound.Read())
+                        return true;
+                    return false;
+                }
+            }
+        }
+    }
+}
